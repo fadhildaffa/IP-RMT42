0f99d82ada0c3d3197d8693f7ff08469c9411c66
fadhildaffa <mdaffadhil98@gmail.com>
2023-11-14 22:42:01 +0700

    authorization

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 const TeamController = require('../controllers/TeamController');
 const UserController = require('../controllers/UserController');
 const authentication = require('../middlewares/authentication');
+const {authorizationAdd, authorizationEditDelete} = require('../middlewares/authorization');
 const errorHandler = require('../middlewares/errorHandler');
 
 const router = require('express').Router();
@@ -14,11 +15,11 @@ router.post('/login', UserController.userLogin)
 
 router.use(authentication)
 router.get('/teams', TeamController.getTeams)
-router.post('/teams', TeamController.createTeam)
+router.post('/teams', authorizationAdd,TeamController.createTeam)
 router.get('/teams/:id', TeamController.findOne)
-router.delete('/teams/:id', TeamController.deleteTeam)
-router.put('/teams/:id', TeamController.updateTeam)
-router.patch('/teams/:id', upload.single('logo'),TeamController.updateLogo)
+router.delete('/teams/:id', authorizationEditDelete, TeamController.deleteTeam)
+router.put('/teams/:id', authorizationEditDelete, TeamController.updateTeam)
+router.patch('/teams/:id', authorizationEditDelete, upload.single('logo'),TeamController.updateLogo)
 
 
 
